de9ad13c704e308fc3ef042801d352c6d71e188a
MitranshAgrawal <mitranshagrawal5@gmail.com>
2025-08-13 18:35:56 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const { act } = require("react")
+
+const accountId = 12345
+let accountemail = "mahi@google.com"
+var accountPassword = "1111"
+accountCity = "Lucknow"
+
+
+accountemail = "mahi222@gmail.com"
+accountPassword = "12345543"
+accountCity = "Mathura"
+
+console.log(accountemail)
+
+console.table([accountId,accountPassword,accountemail,accountCity])
